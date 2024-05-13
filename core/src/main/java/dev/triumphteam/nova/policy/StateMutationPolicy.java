27b37f46805cb2c37580662d69ad9d103f206d19
Matt <matt.mmor@gmail.com>
2024-05-13 22:04:21 +0100

    chore: Make constructor private for policies

--- core/src/main/java/dev/triumphteam/nova/policy/StateMutationPolicy.java
+++ core/src/main/java/dev/triumphteam/nova/policy/StateMutationPolicy.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2024 TriumphTeam
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -49,6 +49,8 @@ public interface StateMutationPolicy {
 
         public static final StateMutationPolicy INSTANCE = new ReferenceEquality();
 
+        private ReferenceEquality() {}
+
         @Override
         public boolean equivalent(final @Nullable Object a, final @Nullable Object b) {
             return a == b;
@@ -62,6 +64,8 @@ public interface StateMutationPolicy {
     final class StructuralEquality implements StateMutationPolicy {
 
         public static final StateMutationPolicy INSTANCE = new StructuralEquality();
+
+        private StructuralEquality() {}
 
         @Override
         public boolean equivalent(final @Nullable Object a, final @Nullable Object b) {
@@ -77,6 +81,8 @@ public interface StateMutationPolicy {
     final class NeverEqual implements StateMutationPolicy {
 
         public static final StateMutationPolicy INSTANCE = new NeverEqual();
+
+        private NeverEqual() {}
 
         @Override
         public boolean equivalent(final @Nullable Object a, final @Nullable Object b) {
