0b73c0caff547270cb4f28b03c16d2ac5c039211
Matt <matt.mmor@gmail.com>
2024-10-18 00:41:32 +0100

    feature: Factory methods for simple states

--- core/src/main/java/dev/triumphteam/nova/MutableState.java
+++ core/src/main/java/dev/triumphteam/nova/MutableState.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2024 TriumphTeam
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,6 +26,7 @@ package dev.triumphteam.nova;
 import dev.triumphteam.nova.builtin.SimpleMutableState;
 import dev.triumphteam.nova.policy.StateMutationPolicy;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * A representation of a {@link State} that is mutable.
@@ -39,6 +40,54 @@ import org.jetbrains.annotations.NotNull;
  * @see SimpleMutableState
  */
 public interface MutableState<T> extends State {
+
+    /**
+     * Creates a new mutable state with the given value.
+     * Using {@link StateMutationPolicy.StructuralEquality}.
+     *
+     * @param value The starting value of the state.
+     * @param <T>   The type of the value.
+     * @return A new {@link MutableState}.
+     */
+    static <T> @NotNull MutableState<@NotNull T> of(final @NotNull T value) {
+        return of(value, StateMutationPolicy.StructuralEquality.INSTANCE);
+    }
+
+    /**
+     * Creates a new mutable state with the given value.
+     *
+     * @param value  The starting value of the state.
+     * @param policy The {@link StateMutationPolicy} to be used.
+     * @param <T>    The type of the value.
+     * @return A new {@link MutableState}.
+     */
+    static <T> @NotNull MutableState<@NotNull T> of(final @NotNull T value, final @NotNull StateMutationPolicy policy) {
+        return new SimpleMutableState<>(value, policy);
+    }
+
+    /**
+     * Creates a new mutable nullable state with the given value.
+     * Using {@link StateMutationPolicy.StructuralEquality}.
+     *
+     * @param value The starting nullable value of the state.
+     * @param <T>   The type of the value.
+     * @return A new {@link MutableState}.
+     */
+    static <T> @NotNull MutableState<@Nullable T> ofNullable(final @Nullable T value) {
+        return ofNullable(value, StateMutationPolicy.StructuralEquality.INSTANCE);
+    }
+
+    /**
+     * Creates a new mutable nullable state with the given value.
+     *
+     * @param value  The starting nullable value of the state.
+     * @param policy The {@link StateMutationPolicy} to be used.
+     * @param <T>    The type of the value.
+     * @return A new {@link MutableState}.
+     */
+    static <T> @NotNull MutableState<@Nullable T> ofNullable(final @Nullable T value, final @NotNull StateMutationPolicy policy) {
+        return new SimpleMutableState<>(value, policy);
+    }
 
     /**
      * Gets the current value of the state.
